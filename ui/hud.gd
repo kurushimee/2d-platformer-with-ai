299b693dfffa50303c65d4b1d4e554d25a56e6f6
kurushimee <jermacoff@gmail.com>
2025-06-19 04:00:08 +0700

    refactor main

--- ui/hud.gd
+++ ui/hud.gd
@@ -3,5 +3,5 @@ extends CanvasLayer
 @onready var game_over_label: Label = $GameOverLabel
 
 
-func show_game_over() -> void:
+func _on_player_died() -> void:
 	game_over_label.show()
--- utilities/managers/game_manager.gd
+++ utilities/managers/game_manager.gd
@@ -1,11 +1,26 @@
 extends Node
 
 var enemies_killed: int = 0
+var is_game_over: bool = false
 
 
 func _ready() -> void:
 	SignalBus.enemy_killed.connect(_on_enemy_killed)
+	get_tree().get_first_node_in_group("player").player_died.connect(_on_player_died)
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_pressed() and is_game_over:
+		restart()
+
+
+func restart() -> void:
+	get_tree().reload_current_scene.call_deferred()
 
 
 func _on_enemy_killed() -> void:
 	enemies_killed += 1
+
+
+func _on_player_died() -> void:
+	is_game_over = true
